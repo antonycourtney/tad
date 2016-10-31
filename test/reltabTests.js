c6bef16edec23b2ece2a39764c6bb24707835edf
Antony Courtney <antony.courtney@gmail.com>
2016-10-31 10:04:13 -0700

    flesh out first reltab test

--- test/reltabTests.js
+++ test/reltabTests.js
@@ -44,11 +44,34 @@ test('reltab filter expressions', (t) => {
 })
 
 test('basic table read', (t) => {
-  t.plan(1)
+  t.plan(5)
   const tq = reltab.tableQuery('test-data/bart-comp-all.json')
   reltab.local.evalQuery(tq).then(res => {
-    console.log('basic table read result: ', res)
     t.ok(true, 'basic table read')
+    var schema = res.schema
+    var expectedCols = ['Name', 'Title', 'Base', 'OT', 'Other', 'MDV', 'ER',
+                      'EE', 'DC', 'Misc', 'TCOE', 'Source', 'Job', 'Union']
+
+    const columns = schema.columns // array of strings
+    // console.log('columns: ', columns)
+
+    t.deepEqual(columns, expectedCols, 'getSchema column ids')
+
+    const columnTypes = columns.map(colId => schema.columnType(colId))
+    var expectedColTypes = ['text', 'text', 'integer', 'integer', 'integer',
+      'integer', 'integer', 'integer', 'integer', 'integer', 'integer', 'text', 'text', 'text']
+
+    t.deepEqual(columnTypes, expectedColTypes, 'getSchema column types')
+
+    const rowData = res.rowData
+    t.equal(rowData.length, 2873, 'q1 rowData.length')
+
+    console.log(rowData[0])
+
+    var expRow0 = ['Crunican, Grace', 'General Manager', 312461, 0, 3846, 19141, 37513,
+      17500, 1869, 7591, 399921, 'MNP', 'Executive Management', 'Non-Represented']
+
+    t.deepEqual(rowData[0], expRow0, 'first row matches expected')
+    t.end()
   })
-  t.end()
 })
