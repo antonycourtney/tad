b27865db8fe3299443a025441b71cc72b71f5616
Antony Courtney <antony.courtney@gmail.com>
2016-12-01 18:56:00 -0800

    Restore show-on-hover to sidebar; move PivotTableModel back in to Grid

--- src/components/AppPane.js
+++ src/components/AppPane.js
@@ -3,28 +3,15 @@
 import * as React from 'react'
 import Sidebar from './Sidebar'
 import Grid from './Grid'
-import PivotTreeModel from '../PivotTreeModel'
-
 /**
  * top level application pane
  */
 export default class AppPane extends React.Component {
-  ptm: PivotTreeModel
-
-  constructor (props: any) {
-    super(props)
-    const appState = this.props.appState
-
-    // This should probably live in this.state...
-    this.ptm = new PivotTreeModel(appState.rtc, appState.baseQuery, [])
-    this.ptm.openPath([])
-  }
-
   render () {
     return (
       <div className='container-fluid full-height main-container'>
         <Sidebar appState={this.props.appState} stateRefUpdater={this.props.stateRefUpdater} />
-        <Grid ptm={this.ptm} />
+        <Grid appState={this.props.appState} stateRefUpdater={this.props.stateRefUpdater} />
       </div>
     )
   }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,6 +2,8 @@
 
 import * as React from 'react'
 import * as epslick from '../epslick'
+import PivotTreeModel from '../PivotTreeModel'
+import * as _ from 'lodash'
 
 /**
  * A wrapper around SlickGrid
@@ -9,9 +11,32 @@ import * as epslick from '../epslick'
  */
 export default class Grid extends React.Component {
   sgv: Object
+  ptm: PivotTreeModel
+
+  constructor (props: any) {
+    super(props)
+    const appState = this.props.appState
+
+    // This should probably live in this.state...
+    this.ptm = new PivotTreeModel(appState.rtc, appState.baseQuery, [])
+    this.ptm.openPath([])
+  }
 
   componentDidMount () {
-    this.sgv = epslick.sgView('#epGrid', this.props.ptm)
+    this.sgv = epslick.sgView('#epGrid', this.ptm)
+  }
+
+  componentWillReceiveProps (props: any) {
+    const prevPivots = this.ptm.getPivots()
+    const pivots = props.appState.vpivots
+
+    console.log('Grid.componentWillReceiveProps: ', prevPivots, pivots)
+
+    if (!(_.isEqual(prevPivots, pivots))) {
+      console.log('new pivots, pivoting...')
+      this.ptm.setPivots(pivots)
+//      this.sgv.refreshFromModel()
+    }
   }
 
   shouldComponentUpdate () {
