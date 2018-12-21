824dd4433bcc4c00f13ccd02cc237f17ff7fe7d4
Antony Courtney <antony.courtney@gmail.com>
2018-12-20 19:17:53 -0800

    moar tests

--- packages/reltab-sqlite/test/basic.test.ts
+++ packages/reltab-sqlite/test/basic.test.ts
@@ -4,8 +4,14 @@ import * as reltabSqlite from "../src/reltab-sqlite";
 import * as tp from "typed-promisify";
 import { textSpanContainsPosition } from "typescript";
 import { delimiter } from "path";
+import * as log from 'loglevel';
+import * as util from './testUtils';
 
-let testCtx : reltabSqlite.SqliteContext;
+const { col, constVal } = reltab;
+
+log.setLevel('debug');
+
+let testCtx: reltabSqlite.SqliteContext;
 
 test("t0 - trivial query generation", () => {
   const q1 = reltab.tableQuery("barttest");
@@ -47,5 +53,58 @@ test("t1 - basic sqlite tableQuery", async () => {
 
   const qres = await testCtx.evalQuery(q1);
   expect(qres).toMatchSnapshot();
+});
+
+const bartTableQuery = reltab.tableQuery('barttest');
+
+test("t2 - basic bart table query", async () => {
+  const qres = await testCtx.evalQuery(bartTableQuery);
+
+  expect(qres).toMatchSnapshot();
+});
+
+test("basic rowcount", async () => {
+  const rowCount = await testCtx.rowCount(bartTableQuery);
+
+  expect(rowCount).toBe(23);
+});
+
+test('basic project operator', async () => {
+  const pcols = ['JobFamily', 'Title', 'Union', 'Name', 'Base', 'TCOE']
+  const q2 = bartTableQuery.project(pcols);
+
+  const qres = await testCtx.evalQuery(q2);
+  expect(qres.schema.columns).toEqual(pcols);
+
+  expect(qres).toMatchSnapshot();
+});
+
+test('basic groupBy', async () => {
+  const q3 = bartTableQuery.groupBy(['JobFamily', 'Title'], ['TCOE'])  // note: [ 'TCOE' ] equivalent to [ [ 'sum', 'TCOE' ] ]
+
+  // testCtx.showQueries = true;
+  const qres = await testCtx.evalQuery(q3);
+
+  const expCols = ['JobFamily', 'Title', 'TCOE'];
+  expect(qres.schema.columns).toEqual(expCols);
+
+  expect(qres.rowData.length).toBe(19);
+
+  const baseRes = await testCtx.evalQuery(bartTableQuery);
+  const tcoeSum = util.columnSum(baseRes, 'TCOE');
+
+  const groupSum = util.columnSum(qres, 'TCOE');
+  expect(groupSum).toBe(tcoeSum);
+
+  expect(qres).toMatchSnapshot();
+});
+
+test('basic filter', async () => {
+  const q5 = bartTableQuery.filter(reltab.and().eq(col('JobFamily'), constVal('Executive Management')));
+
+  const res = await testCtx.evalQuery(q5);
+  expect(res.rowData.length).toBe(4);
+
+  expect(res).toMatchSnapshot();
 });
 
