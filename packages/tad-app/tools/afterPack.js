b85f6afce29c38fc5e029fc23b19c3e1142edf0e
antonycourtney <antony.courtney@gmail.com>
2022-03-01 21:56:44 -0800

    afterPack additions for windows build

--- packages/tad-app/tools/afterPack.js
+++ packages/tad-app/tools/afterPack.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
+const copy = require('recursive-copy');
+
+const WIN_SSL_DIR = '../../../openssl-1.1';
 
 async function afterPack(context) {
   if (context.packager.platform.name === "mac") {
@@ -8,13 +11,20 @@ async function afterPack(context) {
     const { appOutDir } = context;
     const addonPath = path.join(
       appOutDir,
-      "Tad.app/Contents/Resources/app.asar.unpacked/node_modules/node-duckdb/build/Release/node-duckdb-addon.node"
+      "Tad.app/Contents/Resources/app.asar.unpacked/node_modules/ac-node-duckdb/build/Release/node-duckdb-addon.node"
     );
     const { stdout, stderr } = await exec(
       `./tools/dylib-fixup.sh ${addonPath}`
     );
     console.log(stdout);
     console.error(stderr);
+  }
+  if (context.packager.platform.name === "windows") {
+    const { appOutDir } = context;
+    const duckDbTargetDir = path.join(appOutDir, 'resources/app.asar.unpacked/node_modules/ac-node-duckdb/build/Release');
+    const sslBinDir = path.join(WIN_SSL_DIR, 'x64/bin');
+    const results = await copy(sslBinDir, duckDbTargetDir);
+    console.info('afterPack: Copied ' + results.length + ' files from SSL bin dir to target');
   }
 }
 
