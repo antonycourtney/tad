244900576976c1227b7e8f47b49fe28e1ca4326d
Antony Courtney <antony.courtney@gmail.com>
2022-05-31 06:42:20 -0700

    add fixup for 3.0.3

--- packages/tad-app/tools/dylib-fixup.sh
+++ packages/tad-app/tools/dylib-fixup.sh
@@ -10,5 +10,7 @@ cp /usr/local/opt/openssl@3/lib/libcrypto.3.dylib $targetDir
 cp /usr/local/opt/openssl@3/lib/libssl.3.dylib $targetDir
 # finally, fix up libssl's ref to libcrypto:
 sslDylib=$targetDir/libssl.3.dylib
+# sigh -- make this work for either 3.0.2 or 3.0.3:
 install_name_tool -change /usr/local/Cellar/openssl@3/3.0.2/lib/libcrypto.3.dylib @rpath/libcrypto.3.dylib $sslDylib
+install_name_tool -change /usr/local/Cellar/openssl@3/3.0.3/lib/libcrypto.3.dylib @rpath/libcrypto.3.dylib $sslDylib
 echo "dylib-fixup: done."
