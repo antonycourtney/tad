096d95418528e3dd092aff6dac853fd2f59d2c11
Antony Courtney <antony.courtney@gmail.com>
2022-06-11 11:38:58 -0700

    Remove the archive to stop the flood of dependabot PRs on outdated code. Tagged with 'archive-0.9.0' before making this change.

--- archive/README.md
+++
@@ -1,5 +0,0 @@
-Tad 0.10.0 is a major restructuring of the Tad implementation.
-
-This directory contains a snapshot of the Tad implementation as of 0.9.0, before this restructuring work.
-
-This is for reference only, and will probably be removed (or moved to a different branch) in the future.
--- archive/tad-0.9.0/tools/lib/versionInfo.js
+++
@@ -1,5 +0,0 @@
-var fs = require('fs')
-var pack = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
-var electronPack = JSON.parse(fs.readFileSync('./node_modules/electron/package.json', 'utf-8'))
-module.exports.tadVersion = pack.version
-module.exports.electronVersion = electronPack.version
