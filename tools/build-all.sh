9d1d3499ec51a393a490e84b692c8cc0dafafb0b
Antony Courtney <antony.courtney@gmail.com>
2022-12-18 20:04:35 -0800

    Add critical set -e to build scripts

--- tools/build-all.sh
+++ tools/build-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 myDir="$(dirname "$0")"
 source "$myDir/buildUtils.sh"
 
--- tools/build-embedded.sh
+++ tools/build-embedded.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Build just the modules needed for tad packaged as an embeddable component
-
+set -e
 myDir="$(dirname "$0")"
 source "$myDir/buildUtils.sh"
 
