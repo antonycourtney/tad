b6ff68ba3f66e69708809d5059b763ff370e0674
Antony Courtney <antony.courtney@gmail.com>
2023-02-26 20:58:23 -0800

    checkpoint checkin -- hoist deps, upgrade some deps and tweak dylib fixup in failed attempt to get electron-builder to work

--- packages/tad-app/tools/dylib-fixup.sh
+++ packages/tad-app/tools/dylib-fixup.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
 echo "dylib-fixup: Fixing up dylibs in " $@
-install_name_tool -change /usr/local/opt/openssl@3/lib/libcrypto.3.dylib @rpath/libcrypto.3.dylib $@
-install_name_tool -change /usr/local/opt/openssl@3/lib/libssl.3.dylib @rpath/libssl.3.dylib $@
+BREW_PREFIX=$(brew --prefix)
+install_name_tool -change $BREW_PREFIX/opt/openssl@3/lib/libcrypto.3.dylib @rpath/libcrypto.3.dylib $@
+install_name_tool -change $BREW_PREFIX/opt/openssl@3/lib/libssl.3.dylib @rpath/libssl.3.dylib $@
 targetDir=$(dirname $@)
 # duckdbDylib=$targetDir/libduckdb.dylib
-# install_name_tool -change /usr/local/opt/openssl@3/lib/libcrypto.3.dylib @rpath/libcrypto.3.dylib $duckdbDylib
-# install_name_tool -change /usr/local/opt/openssl@3/lib/libssl.3.dylib @rpath/libssl.3.dylib $duckdbDylib
-cp /usr/local/opt/openssl@3/lib/libcrypto.3.dylib $targetDir
-cp /usr/local/opt/openssl@3/lib/libssl.3.dylib $targetDir
+# install_name_tool -change $BREW_PREFIX/opt/openssl@3/lib/libcrypto.3.dylib @rpath/libcrypto.3.dylib $duckdbDylib
+# install_name_tool -change $BREW_PREFIX/opt/openssl@3/lib/libssl.3.dylib @rpath/libssl.3.dylib $duckdbDylib
+cp $BREW_PREFIX/opt/openssl@3/lib/libcrypto.3.dylib $targetDir
+cp $BREW_PREFIX/opt/openssl@3/lib/libssl.3.dylib $targetDir
 # finally, fix up libssl's ref to libcrypto:
 sslDylib=$targetDir/libssl.3.dylib
-# sigh -- make this work for either 3.0.2 or 3.0.3:
-install_name_tool -change /usr/local/Cellar/openssl@3/3.0.2/lib/libcrypto.3.dylib @rpath/libcrypto.3.dylib $sslDylib
-install_name_tool -change /usr/local/Cellar/openssl@3/3.0.3/lib/libcrypto.3.dylib @rpath/libcrypto.3.dylib $sslDylib
+# sigh -- make this work for a variety of openssl versions:
+install_name_tool -change $BREW_PREFIX/Cellar/openssl@3/3.0.2/lib/libcrypto.3.dylib @rpath/libcrypto.3.dylib $sslDylib
+install_name_tool -change $BREW_PREFIX/Cellar/openssl@3/3.0.3/lib/libcrypto.3.dylib @rpath/libcrypto.3.dylib $sslDylib
+install_name_tool -change $BREW_PREFIX/Cellar/openssl@3/3.0.7/lib/libcrypto.3.dylib @rpath/libcrypto.3.dylib $sslDylib
+install_name_tool -change $BREW_PREFIX/Cellar/openssl@3/3.0.8/lib/libcrypto.3.dylib @rpath/libcrypto.3.dylib $sslDylib
 echo "dylib-fixup: done."
